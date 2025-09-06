79484ede341bc911a8774d8412ab126599e2b9b1
AbdulloxAAA <abdullohabduhamidov010@gmail.com>
2025-09-06 23:57:52 +0500

    boshlalarin zaybalchala

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ let modeBtn = document.getElementById("mode-btn");
 
 modeBtn.addEventListener("click", function () {
   if (document.body.className != "dark") {
-    this.firstElementChild.src = "images/header/light.png";
+    this.firstElementChild.src = "images/header/light.svg";
   } else {
-    this.firstElementChild.src = "images/header/dark.png";
+    this.firstElementChild.src = "images/header/dark.svg";
   }
   document.body.classList.toggle("dark");
 });
